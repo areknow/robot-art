edbf12528724902e6a86691897e6155422c3f14e
Arnaud Crowther <arnaud.crowther@dynatrace.com>
2021-05-15 12:57:17 -0400

    feat(api): added firebase and db init

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,16 +1,21 @@
 import * as express from 'express';
-import { Message } from '@robot-art/api-interfaces';
+import admin = require('firebase-admin');
 
 const app = express();
-
-const greeting: Message = { message: 'Welcome to api!' };
-
-app.get('/api', (req, res) => {
-  res.send(greeting);
-});
 
 const port = process.env.port || 3333;
 const server = app.listen(port, () => {
   console.log('Listening at http://localhost:' + port + '/api');
 });
 server.on('error', console.error);
+
+admin.initializeApp({
+  credential: admin.credential.cert({
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY,
+  }),
+  databaseURL: process.env.FIREBASE_DB_URL,
+});
+
+const db = admin.firestore();
