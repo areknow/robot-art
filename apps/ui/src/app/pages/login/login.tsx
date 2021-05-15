7ec1b2532d007103354fb6a54469b094df90e514
Arnaud Crowther <arnaud.crowther@dynatrace.com>
2021-05-15 13:29:21 -0400

    feat(ui): added rough firebase auth

--- apps/ui/src/app/pages/login/login.tsx
+++ apps/ui/src/app/pages/login/login.tsx
@@ -1,3 +1,98 @@
+import {
+  FirebaseAuthConsumer,
+  FirebaseAuthProvider,
+  IfFirebaseAuthed,
+  IfFirebaseAuthedAnd,
+} from '@react-firebase/auth';
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import { useState } from 'react';
+import { environment } from '../../../environments/environment';
+
 export const Login = () => {
-  return <div>login</div>;
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [token, setToken] = useState('');
+
+  const getToken = async () => {
+    return await firebase.auth().currentUser.getIdToken(true);
+  };
+
+  return (
+    <FirebaseAuthProvider {...environment.firebaseConfig} firebase={firebase}>
+      <div>
+        token: {JSON.stringify(token)}
+        <br></br>
+        <input
+          value={email}
+          onChange={(event) => setEmail(event.target.value)}
+          placeholder="email"
+        />
+        <input
+          value={password}
+          onChange={(event) => setPassword(event.target.value)}
+          placeholder="password"
+        />
+        <button
+          onClick={async () => {
+            const user = email === 'Admin' ? 'admin@robots.com' : email;
+            await firebase.auth().signInWithEmailAndPassword(user, password);
+            setToken(await getToken());
+          }}
+        >
+          login
+        </button>
+        <button
+          onClick={async () => {
+            await firebase
+              .auth()
+              .createUserWithEmailAndPassword(email, password);
+            setToken(await getToken());
+          }}
+        >
+          register
+        </button>
+        <button
+          onClick={async () => {
+            const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
+            await firebase.auth().signInWithPopup(googleAuthProvider);
+            setToken(await getToken());
+          }}
+        >
+          Sign In with Google
+        </button>
+        <button
+          onClick={async () => {
+            await firebase.auth().signOut();
+            setToken('');
+          }}
+        >
+          Sign Out
+        </button>
+        <FirebaseAuthConsumer>
+          {({ isSignedIn, user, providerId }) => {
+            return (
+              <pre style={{ height: 300, overflow: 'auto' }}>
+                {JSON.stringify({ isSignedIn, user, providerId }, null, 2)}
+              </pre>
+            );
+          }}
+        </FirebaseAuthConsumer>
+        <div>
+          <IfFirebaseAuthed>
+            {() => {
+              return <div>You are authenticated</div>;
+            }}
+          </IfFirebaseAuthed>
+          <IfFirebaseAuthedAnd
+            filter={({ providerId }) => providerId !== 'anonymous'}
+          >
+            {({ providerId }) => {
+              return <div>You are authenticated with {providerId}</div>;
+            }}
+          </IfFirebaseAuthedAnd>
+        </div>
+      </div>
+    </FirebaseAuthProvider>
+  );
 };
--- apps/ui/src/environments/environment.ts
+++ apps/ui/src/environments/environment.ts
@@ -1,6 +1,13 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// When building for production, this file is replaced with `environment.prod.ts`.
-
 export const environment = {
   production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyAoAYQp2qaUeTnYzDls0UBkK3Qobxezk1o',
+    authDomain: 'react-firebase-test-4cd99.firebaseapp.com',
+    projectId: 'react-firebase-test-4cd99',
+    storageBucket: 'react-firebase-test-4cd99.appspot.com',
+    messagingSenderId: '368421215213',
+    appId: '1:368421215213:web:0215354d675bd49da665e4',
+    databaseURL:
+      'https://react-firebase-test-4cd99-default-rtdb.firebaseio.com/',
+  },
 };
