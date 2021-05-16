5245fa342e587e0288dd52743955eb6714702a2d
Arnaud Crowther <arnaud.crowther@dynatrace.com>
2021-05-16 01:56:32 -0400

    feat(ui): styled global-nav

--- apps/ui/src/app/common/components/global-nav/global-nav.tsx
+++ apps/ui/src/app/common/components/global-nav/global-nav.tsx
@@ -1,19 +1,33 @@
 import { Link } from 'react-router-dom';
+import { ReactComponent as Logo } from '../../../../assets/logo.svg';
+import {
+  StyledMainNav,
+  StyledNavContainer,
+  StyledSecondaryNav,
+} from './styles';
 
 export const GlobalNav = () => {
   return (
-    <nav>
-      <ul>
-        <li>
-          <Link to="/">Gallery</Link>
-        </li>
-        <li>
-          <Link to="/login">Login</Link>
-        </li>
-        <li>
-          <Link to="/admin">Admin</Link>
-        </li>
-      </ul>
-    </nav>
+    <StyledNavContainer>
+      <div>
+        <StyledMainNav>
+          <Logo />
+          <ul>
+            <li>
+              <Link to="/">Gallery</Link>
+            </li>
+            <li>Results</li>
+          </ul>
+        </StyledMainNav>
+        <StyledSecondaryNav>
+          <ul>
+            <li>
+              <Link to="/admin">Admin</Link>
+            </li>
+            <li>Log Out</li>
+          </ul>
+        </StyledSecondaryNav>
+      </div>
+    </StyledNavContainer>
   );
 };
---
+++ apps/ui/src/app/common/components/global-nav/styles.ts
@@ -0,0 +1,58 @@
+import styled from 'styled-components';
+
+export const StyledNavContainer = styled.div`
+  background: #fff;
+  box-shadow: 0px 1px 0px rgba(0, 0, 0, 0.17);
+  padding: 24px 0;
+  > div {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    max-width: 1200px;
+    margin: auto;
+  }
+  ul {
+    list-style: none;
+    margin: 0;
+    padding: 0;
+    display: flex;
+    align-items: center;
+    li {
+      a {
+        color: inherit;
+        text-decoration: none;
+      }
+    }
+  }
+`;
+
+export const StyledMainNav = styled.div`
+  display: flex;
+  align-items: center;
+  font-weight: bold;
+  font-size: 18px;
+  line-height: 21px;
+  color: #414242;
+  svg {
+    height: 32px;
+    width: 81px;
+    margin-right: 56px;
+  }
+  ul {
+    li:not(:last-child) {
+      margin-right: 40px;
+    }
+  }
+`;
+export const StyledSecondaryNav = styled.div`
+  font-size: 16px;
+  line-height: 18px;
+  color: #737475;
+  ul {
+    li {
+      &:not(:last-child) {
+        margin-right: 24px;
+      }
+    }
+  }
+`;
